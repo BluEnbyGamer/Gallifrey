9f09c451fb0b56695f1f2195b6459e2f5e2ea04f
BluEnbyGamer <2001allieonix@gmail.com>
2026-07-14 23:38:08 +0100

    Initial commit

---
+++ src/main/java/com/timelordmod/gallifrey/GallifreyModClient.java
@@ -0,0 +1,10 @@
+package com.timelordmod.gallifrey;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class GallifreyModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
