1b443fa500524fef1125d4157a064d1a295be574
LightSpeeddd <speedlight003@gmail.com>
2026-07-25 17:51:09 +0100

    Fixed Texture & Transparency

--- src/main/java/com/timelordmod/gallifrey/GallifreyModClient.java
+++ src/main/java/com/timelordmod/gallifrey/GallifreyModClient.java
@@ -1,10 +1,19 @@
 package com.timelordmod.gallifrey;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
+import net.minecraft.client.render.RenderLayer;
+import com.timelordmod.gallifrey.block.GallifreyModBlocks;
+
 
 public class GallifreyModClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
 
+        BlockRenderLayerMap.INSTANCE.putBlock(GallifreyModBlocks.GALLIFREY_FERN, RenderLayer.getCutout());
+        BlockRenderLayerMap.INSTANCE.putBlock(GallifreyModBlocks.GALLIFREY_GRASS, RenderLayer.getCutout());
+        BlockRenderLayerMap.INSTANCE.putBlock(GallifreyModBlocks.GALLIFREY_SAP, RenderLayer.getCutout());
+        BlockRenderLayerMap.INSTANCE.putBlock(GallifreyModBlocks.GALLIFREY_LEAVES, RenderLayer.getCutoutMipped());
     }
 }
+
