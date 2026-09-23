79908c5983f30b950f2aa32ba29639b6d2e81285
DiceLine <alfieharrylansdell@gmail.com>
2026-09-23 17:01:53 +0100

    WHatever

--- src/client/java/com/timelordmod/gallifrey/mixin/client/PlayerEntityRendererMixin.java
+++ src/client/java/com/timelordmod/gallifrey/mixin/client/PlayerEntityRendererMixin.java
@@ -1,28 +1,21 @@
 package com.timelordmod.gallifrey.mixin.client;
 
 import com.timelordmod.gallifrey.client.HatFeatureRenderer;
-import net.minecraft.client.network.AbstractClientPlayerEntity;
+import com.timelordmod.gallifrey.client.LivingEntityRendererAccessor;
 import net.minecraft.client.render.entity.PlayerEntityRenderer;
-import net.minecraft.client.render.entity.feature.FeatureRenderer;
-import net.minecraft.client.render.entity.model.PlayerEntityModel;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import java.util.List;
-
 @Mixin(PlayerEntityRenderer.class)
 public abstract class PlayerEntityRendererMixin {
 
-    @Shadow
-    @Final
-    private List<FeatureRenderer<AbstractClientPlayerEntity, PlayerEntityModel<AbstractClientPlayerEntity>>> features;
-
     @Inject(method = "<init>", at = @At("TAIL"))
     private void gallifrey$addHeadwearFeature(CallbackInfo ci) {
-        features.add(new HatFeatureRenderer((PlayerEntityRenderer) (Object) this));
+        LivingEntityRendererAccessor accessor = (LivingEntityRendererAccessor) (Object) this;
+        accessor.gallifrey$getFeatures().add(
+                new HatFeatureRenderer((PlayerEntityRenderer) (Object) this)
+        );
     }
 }
