63d5ca412b6ca0ca89d7fcff71da301c5d960e66
BluEnbyGamer <2001allieonix@gmail.com>
2026-09-23 23:06:19 +0100

    Remerged the client and server side

---
+++ src/main/java/com/timelordmod/gallifrey/mixin/client/PlayerEntityRendererMixin.java
@@ -0,0 +1,20 @@
+package com.timelordmod.gallifrey.mixin.client;
+
+import com.timelordmod.gallifrey.client.HatFeatureRenderer;
+import net.minecraft.client.render.entity.PlayerEntityRenderer;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(PlayerEntityRenderer.class)
+public abstract class PlayerEntityRendererMixin {
+
+    @Inject(method = "<init>", at = @At("TAIL"))
+    private void gallifrey$addHeadwearFeature(CallbackInfo ci) {
+        LivingEntityRendererAccessor accessor = (LivingEntityRendererAccessor) (Object) this;
+        accessor.gallifrey$getFeatures().add(
+                new HatFeatureRenderer((PlayerEntityRenderer) (Object) this)
+        );
+    }
+}
