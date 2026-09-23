d10c53a85157cc9f759158691cd6315b6f54e92d
DiceLine <alfieharrylansdell@gmail.com>
2026-09-23 16:43:01 +0100

    i give up

--- src/client/java/com/timelordmod/gallifrey/mixin/client/PlayerEntityRendererMixin.java
+++ src/client/java/com/timelordmod/gallifrey/mixin/client/PlayerEntityRendererMixin.java
@@ -5,20 +5,28 @@ import net.minecraft.client.network.AbstractClientPlayerEntity;
 import net.minecraft.client.render.entity.PlayerEntityRenderer;
 import net.minecraft.client.render.entity.feature.FeatureRenderer;
 import net.minecraft.client.render.entity.model.PlayerEntityModel;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.gen.Invoker;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import java.util.List;
+
 @Mixin(PlayerEntityRenderer.class)
 public abstract class PlayerEntityRendererMixin {
-    @Invoker("addFeature")
-    protected abstract boolean gallifrey$addFeature(
-            FeatureRenderer<AbstractClientPlayerEntity, PlayerEntityModel<AbstractClientPlayerEntity>> feature);
+    /*
+     * addFeature() is declared on LivingEntityRenderer, not PlayerEntityRenderer.
+     * An @Invoker on this mixin therefore cannot resolve the method. Shadow the
+     * inherited features list instead and add the custom feature directly.
+     */
+    @Shadow
+    @Final
+    protected List<FeatureRenderer<AbstractClientPlayerEntity, PlayerEntityModel<AbstractClientPlayerEntity>>> features;
 
     @Inject(method = "<init>", at = @At("TAIL"))
     private void gallifrey$addHeadwearFeature(CallbackInfo ci) {
-        gallifrey$addFeature(new HatFeatureRenderer((PlayerEntityRenderer) (Object) this));
+        features.add(new HatFeatureRenderer((PlayerEntityRenderer) (Object) this));
     }
 }
