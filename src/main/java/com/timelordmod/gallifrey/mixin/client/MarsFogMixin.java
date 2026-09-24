066a9f165567a3df2a34a8e9d53db153204f32f0
DiceLine <alfieharrylansdell@gmail.com>
2026-09-24 16:19:17 +0100

    mars

---
+++ src/main/java/com/timelordmod/gallifrey/mixin/client/MarsFogMixin.java
@@ -0,0 +1,26 @@
+package com.timelordmod.gallifrey.mixin.client;
+
+import com.mojang.blaze3d.systems.RenderSystem;
+import net.minecraft.client.render.BackgroundRenderer;
+import net.minecraft.client.render.Camera;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(BackgroundRenderer.class)
+public class MarsFogMixin {
+    @Inject(method = "applyFog", at = @At("HEAD"), cancellable = true)
+    private static void gallifrey$marsFog(Camera camera, BackgroundRenderer.FogType fogType,
+                                           float viewDistance, boolean thickFog, float tickDelta,
+                                           CallbackInfo ci) {
+        if (camera.getFocusedEntity() == null) return;
+        World world = camera.getFocusedEntity().getWorld();
+        if ("mars".equals(world.getRegistryKey().getValue().getPath())) {
+            RenderSystem.setShaderFogStart(4.0F);
+            RenderSystem.setShaderFogEnd(Math.min(viewDistance, 96.0F));
+            ci.cancel();
+        }
+    }
+}
